1b6982e6c75be097de87985d6c8011c18b95d0a7
Francisco Perez <pacogppl@hotmail.com>
2017-01-18 00:24:49 +0100

    Problem statement

--- Problem.py
+++ Problem.py
@@ -30,16 +30,24 @@ class Problem(object):
         vehicleId = 0
 
         vehicles = []      
-        vehicles = []      
+        #the constraints are the first one to insert into the algorithm
+    #first I need to create the matrix that represents if a locations has been visited or not
+    #as in cplex with constraint 1 and 2
+    #forall(l1 in L1)
+    #   sum(l2 in L)xl[l1,l2]==1;
+    #forall(l1 in L)
+    #   sum(l2 in L)xl[l1,l2]-sum(l2 in L)xl[l2,l1]==0;
+    #we have to create the conditions to create the arrival time as in cplex
+    #distance must be in the time window()
+    #each location should be visited only once
+
+    vehicles = []      
     xl = numpy.zeros((nLocations, nLocations))
     arrivalTime = [0] * (nLocations-1)
-    #waitTime = [0] * (nLocations-1)
     vehicleId = 0
     vehi = Vehicle(vehicleId)
 
     #closestOne stores the index(min_index) and value (min_value) of the closest cities
-    # arrivalTime = [] 
-    # waitTime = []
     closestOne=[]
     min_index=0
     min_value=0
@@ -48,8 +56,8 @@ class Problem(object):
     visitedLocations=[0] 
     locationsDestination=[] 
     visitedLocationsDestination=[] 
-    indexer = []
 
+    #array with all the destinations
     for indexDestiny in xrange(0, nLocations): 
         dest_value = dist[indexDestiny][0]
         locationsDestination.append(dest_value)
@@ -66,19 +74,20 @@ class Problem(object):
             min_index+=1  
 
         #checks if the location has already been visited
-        if(min_index in visitedLocations):
-            setRepeated = range(0, nLocations)
-            setNotRepeated =list(set(range(1, nLocations))-set(visitedLocations))
-            print "setRepeated: ",setRepeated,"setNotRepeated: ",setNotRepeated
-            randomElement = random.choice(setNotRepeated)
-            print "randomElement: ",randomElement
-            positions = [x for j,x in enumerate(dist[randomElement]) if j!=0]     
-            min_index = randomElement 
-            min_value = min(k for k in positions if k > 0)    
-            #min_index, min_value = min(enumerate(k for k in positions if k > 0), key=operator.itemgetter(1))
-            print "positions: ",positions
-            print "min_index repeated: ", min_index
-            print "min_value repeated: ", min_value
+        if(len(visitedLocations)!=nLocations):
+            if(min_index in visitedLocations):
+                setRepeated = range(0, nLocations)
+                setNotRepeated =list(set(range(1, nLocations))-set(visitedLocations))
+                print "setRepeated: ",setRepeated,"setNotRepeated: ",setNotRepeated
+                randomElement = random.choice(setNotRepeated)
+                print "randomElement: ",randomElement
+                positions = [x for j,x in enumerate(dist[randomElement]) if j!=0]     
+                min_index = randomElement 
+                min_value = min(k for k in positions if k > 0)    
+                #min_index, min_value = min(enumerate(k for k in positions if k > 0), key=operator.itemgetter(1))
+                print "positions: ",positions
+                print "min_index repeated: ", min_index
+                print "min_value repeated: ", min_value
 
         #if its already visited, go back index and continue with another
         print "min_index: ", min_index
@@ -88,68 +97,59 @@ class Problem(object):
         #atime=0 represents that a new vehicle has been send to a route
         #so we can sum the previous value of arrivalTime to aTime
         if(aTime==0):
-            print "timeWindow 1: ",timeWindow[min_index][0]
+            print "timeWindow 1: ",timeWindow[min_index-1][0]
             #if the lower[0] timeWidow is bigger that the distance, the arrivalTime is the 
             # sum of the lower timeWidow and the tl
-            if(timeWindow[min_index][0]>=min_value):
-                aTime = timeWindow[min_index][0] + tl[min_index-1] 
+            if(timeWindow[min_index-1][0]>=min_value):
+                aTime = timeWindow[min_index-1][0] + tl[min_index-1] 
                 print "tl 1: ", tl[min_index-1]
                 print "enter ",indexX 
                 print "aTime 1: ",aTime   
-                print "tw: ",timeWindow[min_index][0]
+                print "tw: ",timeWindow[min_index-1][0]
             else:
                 aTime = min_value + tl[min_index-1] 
                 print "tl 2: ", tl[min_index-1]
                 print "enter ",indexX 
                 print "aTime 2: ",aTime  
-                print "tw: ",timeWindow[min_index][0]
+                print "tw: ",timeWindow[min_index-1][0]
         else:
             #if the time is upper the maxTime, create a new car and reassing the route until it fits
             if(aTime<=maxTime):
-                print "timeWindow 2: ",timeWindow[min_index][0]
+                print "timeWindow 2: ",timeWindow[min_index-1][0]
                 #if the lower[0] timeWidow is bigger that the distance, the arrivalTime is the 
                 # sum of the lower timeWidow and the tl
-                if(timeWindow[min_index][0]>=min_value):
-                    aTime = arrivalTime[indexX-1] + timeWindow[min_index][0] + tl[min_index-1]
+                if(timeWindow[min_index-1][0]>=min_value):
+                    aTime = arrivalTime[indexX-1] + timeWindow[min_index-1][0] + tl[min_index-1]
                     print "tl 3: ", tl[min_index-1] 
                     print "enter ",indexX 
                     print "aTime 3: ",aTime  
-                    print "tw: ",timeWindow[min_index][0]
+                    print "tw: ",timeWindow[min_index-1][0]
                 else:
                     aTime = arrivalTime[indexX-1] + min_value + tl[min_index-1] 
                     print "tl 4: ", tl[min_index-1]
                     print "enter ",indexX 
                     print "aTime 4: ",aTime  
-                    print "tw: ",timeWindow[min_index][0]
+                    print "tw: ",timeWindow[min_index-1][0]
             else:
-                aTime = 0                
+                print "locationsDestination ", "[", min_index,"]",locationsDestination[min_index]  
+                aTime = arrivalTime[indexX-1] + locationsDestination[min_index]   
+                print "aTime: ",aTime
                 vehicles.append(vehi)
                 vehicleId += 1
                 vehi = Vehicle(vehicleId)
-                print "vehi",vehi.getLocationIds()
-                print "vehicles",vehicles
-                #visitedLocationsDestination.append()
-                # min_index = 
-                # min_value = 
-                # locationsDestination[positionX]        
+                aTime =0       
                 
         closestOne.append([min_index, min_value])   
         visitedLocations.append(min_index)       
         positionX = min_index
         print "position X: ", positionX
-        arrivalTime[indexX]=aTime
+        if (aTime!=0):
+            print indexX
+            arrivalTime[indexX-1]=aTime
         vehi.addLocationToVehicle(vehicleId,min_index)
 
     def getVehicles(self):
         return(self.vehicles)
 
-    def getTimeWindow(self):
-        return(self.timeWindow[0+1][0])
-
-    print timeWindow[7][0]
-    print "arrivalTime: ",arrivalTime
-    print "waitTime: ",waitTime
-    print "vehicleId: ",vehicleId
-    print "minimun of all: ", closestOne
-    print "min_index: ", min_index
-    print "min_value: ", min_value
+    def getTimeWindow(self,posX,posY):
+        return(self.timeWindow[posX][posY])
